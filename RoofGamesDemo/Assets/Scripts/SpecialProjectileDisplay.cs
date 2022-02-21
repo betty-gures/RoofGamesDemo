32252c37598317b84281c60335218c18507aaa5e
Betul Gures <fatma.betul.gures@gmail.com>
2022-02-21 12:00:19 +0300

    Upload of Demo
    First Upload of the demo on GitHub

---
+++ RoofGamesDemo/Assets/Scripts/SpecialProjectileDisplay.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpecialProjectileDisplay : MonoBehaviour
+{
+
+    Text specialProjectileText;
+    Player player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       specialProjectileText = GetComponent<Text>();
+       player = FindObjectOfType<Player>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        specialProjectileText.text= player.GetSpecialProjectile().ToString();
+    }
+}
